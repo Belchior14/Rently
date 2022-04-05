c287450fb2a3bee9e0a5a3844abda3e133085bda
Belchior <belchior.fontao@gmail.com>
2022-04-05 19:43:05 +0100

    first commit

---
+++ models/User.model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: String,
+  },
+  score: {
+    type: String,
+  },
+  products: {
+    type: String,
+  },
+  comments: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  money: {
+    type: String,
+  },
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
