245150f62d1d3317315e67a58f643f1d28644e88
Belchior <belchior.fontao@gmail.com>
2022-04-09 21:28:40 +0100

    added the populate(products) at the profile route

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,34 +1,14 @@
 const express = require("express");
 const { json } = require("express/lib/response");
-const Product = require("../models/Product.model");
 const User = require("../models/User.model");
 
 const router = express.Router();
 
 //route to see the user profile
-router.get("/:id" , async (req,res)=> {
-
-    const {id} = req.params
-
-    const user = await User.findById(id)
-
-    const products = await Product.find({user: req.jwtPayload.user._id})
-    console.log(products)
-
-    res.status(200).json(user)
-
-})
-
-
-module.exports = router;
-
-/* //home route
-router.get("/home", async (req, res) => {
-    const product = await Product.find();
-    res.status(200).json(product);
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id).populate("products");
+  res.status(200).json(user);
 });
 
-//about route
-router.get("/about", async (req, res) => {
-    res.status(200).json();
-}); */
+module.exports = router;
