966b2d8d1bf8d27f343a413461c386552ab4f161
Cristina Quijano <cristina@cristinas-air-2.kpn>
2022-04-09 09:39:06 +0200

    home and about routes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,6 +3,15 @@ const User = require("../models/User.model");
 
 const router = express.Router();
 
-//
+//home route
+router.get("/home", async (req, res) => {
+    const product = await Product.find();
+    res.status(200).json(product);
+});
+
+//about route
+router.get("/about", async (req, res) => {
+    res.status(200).json();
+});
 
 module.exports = router;
