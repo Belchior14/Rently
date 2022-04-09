8f4bf834683c43201a8000734edd5e9fcc28ae4a
Belchior <belchior.fontao@gmail.com>
2022-04-09 20:50:48 +0100

    begin of the profile route

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const { json } = require("express/lib/response");
+const Product = require("../models/Product.model");
 const User = require("../models/User.model");
 
 const router = express.Router();
 
-//home route
+//route to see the user profile
+router.get("/:id" , async (req,res)=> {
+
+    const {id} = req.params
+
+    const user = await User.findById(id)
+
+    const products = await Product.find({user: req.jwtPayload.user._id})
+    console.log(products)
+
+    res.status(200).json(user)
+
+})
+
+
+module.exports = router;
+
+/* //home route
 router.get("/home", async (req, res) => {
     const product = await Product.find();
     res.status(200).json(product);
@@ -12,6 +31,4 @@ router.get("/home", async (req, res) => {
 //about route
 router.get("/about", async (req, res) => {
     res.status(200).json();
-});
-
-module.exports = router;
+}); */
