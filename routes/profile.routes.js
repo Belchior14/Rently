fbd399d0f306eb6b6e88cf5277feedef7abd57a2
Belchior <belchior.fontao@gmail.com>
2022-04-12 19:50:15 +0100

    profile pages changes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,22 +1,15 @@
 const express = require("express");
-const { json } = require("express/lib/response");
 const User = require("../models/User.model");
-const Product = require("../models/Product.model")
 
 const router = express.Router();
 
 //profile page
 router.get("/:id", async (req, res) => {
     const {id} = req.params;
-    const user = await User.findById(id);
-    const product = await Product.find();
+    const user = await User.findById(id).populate("products").populate("rentedProducts");
+    
     res.status(200).json(user);
   });
 
-//home route
-router.get("/home", async (req, res) => {
-    const product = await Product.find();
-    res.status(200).json(product);
-});
 
 module.exports = router;
