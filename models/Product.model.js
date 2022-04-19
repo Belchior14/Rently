ae8e95cdc76ccb9bd38b94919d0881d558cedbf7
Belchior <belchior.fontao@gmail.com>
2022-04-19 20:58:34 +0100

    added the rented status to the Product Model

--- models/Product.model.js
+++ models/Product.model.js
@@ -50,6 +50,11 @@ const productSchema = new Schema({
   },
   scheduledDates:{
     type:[String]
+  },
+  rented:{
+    type: Boolean,
+    default: false
+
   }
 });
 
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -81,6 +81,8 @@ router.post("/rent/:id",authenticate, async (req, res) => {
   const product = await Product.findById(id); //find the product selected
   // the user can only rent a product that nor belong to them
   if (product.user.toString() !== req.jwtPayload.user._id) {
+    product.rented = !product.rented
+    await product.save()
     const user = await User.findById(req.jwtPayload.user._id); // find the user that is logged in
     const price = product.price; // set the price of the product
     const rentUserId = product.user; // set the ID of the person who is renting the product
