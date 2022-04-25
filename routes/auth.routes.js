b2bf82e6344de14aea358ea92a6e310fae1271e4
Belchior <belchior.fontao@gmail.com>
2022-04-25 11:30:18 +0100

    new sign up route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,31 +7,61 @@ const router = express.Router();
 
 //route to create an user
 router.post("/signup", async (req, res) => {
-  const { firstName, lastName, email, password, username} = req.body;
+  const { firstName, lastName, email, password, username } = req.body;
 
-  const userFound = await User.findOne({email, username})
-  if(!userFound){
-
-    try {
-      const passwordHash = await bcrypt.hash(password, 10);
-      const user = await User.create({
-        firstName,
-        lastName,
-        email,
-        username,
-        password: passwordHash,
-      });
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(409).json("Email already exists");
-    }
+  if (
+    email === "" ||
+    password === "" ||
+    firstName === "" ||
+    lastName === "" ||
+    username === ""
+  ) {
+    res.status(400).json({
+      message: "Provide email, password, first and last name, and the username",
+    });
+    return;
   } else {
-    res.status(500).json("email or username already exists");
+    // Use regex to validate the email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    if (!emailRegex.test(email)) {
+      res.status(400).json({ message: "Provide a valid email address." });
+      return;
+    }
+
+    // Use regex to validate the password format
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!passwordRegex.test(password)) {
+      res.status(400).json({
+        message:
+          "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+      });
+      return;
+    }
+    const userFound = await User.findOne({ email, username });
+    if (!userFound) {
+      try {
+        const passwordHash = await bcrypt.hash(password, 10);
+        const user = await User.create({
+          firstName,
+          lastName,
+          email,
+          username,
+          password: passwordHash,
+        });
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(400).json({
+          message:
+            "Email/username already exists",
+        });
+      }
+    } else {
+      res.status(400).json({ message: "Email/username already exists" });
+    }
   }
-  
 });
 
-//route to log in 
+//route to log in
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -63,9 +93,9 @@ router.post("/login", async (req, res) => {
 
 //route to see all the users
 
-router.get("/" ,async (req,res)=> {
-  const users = await User.find()
-  res.status(200).json(users)
-})
+router.get("/", async (req, res) => {
+  const users = await User.find();
+  res.status(200).json(users);
+});
 
 module.exports = router;
