603edf9f7300f1d572b77e12c47ba93d2534590c
Belchior <belchior.fontao@gmail.com>
2022-05-04 18:23:12 +0100

    edit profile image

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,22 +3,38 @@ const User = require("../models/User.model");
 
 const router = express.Router();
 
-
 //profile page
 router.get("/:id", async (req, res) => {
-    const {id} = req.params;
-    const user = await User.findById(id).populate("products").populate("rentedProducts");
-    
-    res.status(200).json(user);
-  });
+  const { id } = req.params;
+  const user = await User.findById(id)
+    .populate("products")
+    .populate("rentedProducts");
 
-  router.put("/:id", async (req,res) => {
-    const {id} = req.params;
-    const user = await User.findById(id).populate("products").populate("rentedProducts");
-    user.money += 100;
-    await user.save()
-    res.status(200).json(user)
-  })
+  res.status(200).json(user);
+});
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id)
+    .populate("products")
+    .populate("rentedProducts");
+  user.money += 100;
+  await user.save();
+  res.status(200).json(user);
+});
+
+router.put("/id", async (req, res) => {
+  const { id } = req.params;
+  const { image } = req.body;
+  const user = await User.findById(id);
+  console.log(image)
+  if (user_id === req.jwtPayload.user._id) {
+    user.image = image;
+    await user.save();
+    res.status(200).json("User edited with sucess!");
+  } else {
+    res.status(401).json("You are not the user");
+  }
+});
 
 module.exports = router;
